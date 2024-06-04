47fae1734248564d907efc453b40688fc6364636
Robin Le Floch <robin.le.floch@gmail.com>
2024-06-04 10:07:12 +0200

    ajout de prepare dataset + poetry run black sur spectogram

--- koregraph/api/spectogram.py
+++ koregraph/api/spectogram.py
@@ -11,27 +11,39 @@ import matplotlib.pyplot as plt
 
 environ["IMAGE_FOLDER"] = Path(environ.get("IMAGE_FOLDER", "images"))
 
+
 def save_images_log_power_spectogram(file_path: Path):
-    '''Save log power spectogram image of the audio file
+    """Save log power spectogram image of the audio file
 
     Args:
         file_path (Path) : path to the audio file
 
     Returns:
         Path: path to the saved image
-    '''
+    """
 
-    image_folder = environ.get("IMAGE_FOLDER")
+    image_folder = Path(environ.get("IMAGE_FOLDER"))
     Path(image_folder).mkdir(parents=True, exist_ok=True)
 
-    y, sr = load(file_path, duration=10) # load the first 10 seconds of the audio file
+    y, sr = load(file_path, duration=10)  # load the first 10 seconds of the audio file
 
     fig, ax = plt.subplots(figsize=(50, 10))
-    melspectrogram = feature.melspectrogram(y=y, sr=sr,n_fft=512, n_mels=128, hop_length=128)
-    imgdb = display.specshow(power_to_db(melspectrogram, ref=np.max), sr=sr, y_axis='mel', x_axis='time', ax=ax, cmap='gray_r')
-    ax.set(title='Log-Power spectrogram')
+    melspectrogram = feature.melspectrogram(
+        y=y, sr=sr, n_fft=512, n_mels=128, hop_length=128
+    )
+    imgdb = display.specshow(
+        power_to_db(melspectrogram, ref=np.max),
+        sr=sr,
+        y_axis="mel",
+        x_axis="time",
+        ax=ax,
+        cmap="gray_r",
+    )
+    ax.set(title="Log-Power spectrogram")
     fig.colorbar(imgdb, ax=ax, format="%+2.0f dB")
-    image_path = Path(image_folder) / f"{path.basename(file_path)}_log_power_spectrogram.png"
+    image_path = (
+        Path(image_folder) / f"{path.basename(file_path)}_log_power_spectrogram.png"
+    )
     plt.savefig(image_path)
     plt.close()
 
