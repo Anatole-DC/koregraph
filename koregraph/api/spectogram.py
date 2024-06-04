049911b68568981c3dcbd8e8913e56cf689afbcd
Robin Le Floch <robin.le.floch@gmail.com>
2024-06-04 09:16:45 +0200

    Extract to global environment variable ok +  Use pathlib.Path ok

--- koregraph/api/spectogram.py
+++ koregraph/api/spectogram.py
@@ -1,12 +1,15 @@
-import os
-import pathlib
-import librosa
-import numpy as np
+from os import path
+from os import environ
 from pathlib import Path
+from librosa import load
+from librosa import display
+from librosa import power_to_db
+from librosa import feature
 
+import numpy as np
 import matplotlib.pyplot as plt
 
-os.environ["IMAGE_FOLDER"] = '../data/images'
+environ["IMAGE_FOLDER"] = Path(environ.get("IMAGE_FOLDER", "images"))
 
 def save_images_log_power_spectogram(file_path: Path):
     '''Save log power spectogram image of the audio file
@@ -18,20 +21,18 @@ def save_images_log_power_spectogram(file_path: Path):
         Path: path to the saved image
     '''
 
-    image_folder = os.environ.get("IMAGE_FOLDER")
-    pathlib.Path(image_folder).mkdir(parents=True, exist_ok=True)
+    image_folder = environ.get("IMAGE_FOLDER")
+    Path(image_folder).mkdir(parents=True, exist_ok=True)
 
-    y, sr = librosa.load(file_path, duration=10) # load the first 10 seconds of the audio file
+    y, sr = load(file_path, duration=10) # load the first 10 seconds of the audio file
 
     fig, ax = plt.subplots(figsize=(50, 10))
-    melspectrogram = librosa.feature.melspectrogram(y=y, sr=sr,n_fft=512, n_mels=128, hop_length=128)
-    imgdb = librosa.display.specshow(librosa.power_to_db(melspectrogram, ref=np.max), sr=sr, y_axis='mel', x_axis='time', ax=ax, cmap='gray_r')
+    melspectrogram = feature.melspectrogram(y=y, sr=sr,n_fft=512, n_mels=128, hop_length=128)
+    imgdb = display.specshow(power_to_db(melspectrogram, ref=np.max), sr=sr, y_axis='mel', x_axis='time', ax=ax, cmap='gray_r')
     ax.set(title='Log-Power spectrogram')
     fig.colorbar(imgdb, ax=ax, format="%+2.0f dB")
-    image_path = pathlib.Path(image_folder) / f"{os.path.basename(file_path)}_log_power_spectrogram.png"
+    image_path = Path(image_folder) / f"{path.basename(file_path)}_log_power_spectrogram.png"
     plt.savefig(image_path)
     plt.close()
 
-    return pathlib.Path(f"{image_folder}/{os.path.basename(file_path)}_log_power_spectrogram.png")
-
-# save_images_log_power_spectogram("../data/music/mp3/mBR0.mp3")
+    return Path(f"{image_folder}/{path.basename(file_path)}_log_power_spectrogram.png")
