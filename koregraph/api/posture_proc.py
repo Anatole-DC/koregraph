1e408f11d12f482af0219da505db4f77614ea7dd
kelly anne <kdanne206@gmail.com>
2024-06-04 13:10:58 +0200

    Fix function, remove len, rename

--- koregraph/api/posture_proc.py
+++ koregraph/api/posture_proc.py
@@ -5,7 +5,7 @@ from pickle import load as load_pickle
 from typing import Tuple
 
 
-def generate_pickle_and_cut_audio(choregraphy_name: str) -> Tuple[array, int]:
+def generate_posture_array(choregraphy_name: str) -> array:
     """Create a numpy array with 34 columns
 
     Args:
@@ -13,10 +13,9 @@ def generate_pickle_and_cut_audio(choregraphy_name: str) -> Tuple[array, int]:
 
     Returns:
         Array of positions: The postures 34 columns N rows.
-        Number of frame to use in the audio
     """
     with open(KEYPOINTS_DIRECTORY / (choregraphy_name + ".pkl"), "rb") as f:
         data = load_pickle(f)
         postures = data["keypoints2d"][0, :, :, :2]
 
-    return postures.reshape(-1, 34), len(data["timestamps"])
+    return postures.reshape(-1, 34)
