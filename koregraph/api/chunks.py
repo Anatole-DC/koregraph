207a89ef2e1fd18c4f255ead128d841e225c7ef1
kelly anne <kdanne206@gmail.com>
2024-06-04 11:09:20 +0200

    Create api file

---
+++ koregraph/api/chunks.py
@@ -0,0 +1,39 @@
+from koregraph.tools.choregraphy_to_chunks import split_sequence, split_audio
+from koregraph.managers.choregraphy import save_choregaphy_chunk
+from koregraph.managers.audio import save_audio_chunk
+from koregraph.params import GENERATED_AUDIO_DIRECTORY, GENERATED_KEYPOINTS_DIRECTORY
+
+import os
+import shutil
+
+
+def clean_path(path, reload=True):
+    if os.path.exists(path) and not os.path.isfile(path) and reload:
+        try:
+            shutil.rmtree(path)
+        except Exception as e:
+            print(f"Failed to delete {path}. Reason: {e}")
+    path.mkdir(parents=True, exist_ok=True)
+
+
+def generate_chunk(choregraphy_name: str, chunk_size: int, reload_music: bool = False):
+    GENERATED_KEYPOINTS_DIRECTORY.mkdir(parents=True, exist_ok=True)
+    GENERATED_AUDIO_DIRECTORY.mkdir(parents=True, exist_ok=True)
+
+    # Clean previous chunks out if needed
+    chore_path = GENERATED_KEYPOINTS_DIRECTORY / choregraphy_name / chunk_size
+    clean_path(chore_path)
+    _, _, _, _, music_name, _ = choregraphy_name.split("_")
+    music_path = GENERATED_AUDIO_DIRECTORY / music_name / chunk_size
+    clean_path(music_path, reload_music)
+
+    # Get and save chunks
+    chores = split_sequence(choregraphy_name, int(chunk_size))
+    for chore in chores:
+        save_choregaphy_chunk(chore, chore_path)
+
+    if reload_music or len(os.listdir(music_path)) == 0:
+        musics, sr = split_audio(choregraphy_name, chunk_size_sec=int(chunk_size))
+        for music, chunk_id in musics:
+            path = music_path / (music_name + f"_{chunk_id}.mp3")
+            save_audio_chunk(music, sr, path)
--- koregraph/cli/chunk.py
+++ koregraph/cli/chunk.py
@@ -1,12 +1,7 @@
 from argparse import ArgumentParser, BooleanOptionalAction
 from pathlib import Path
-import os
-import shutil
 
-from koregraph.tools.choregraphy_to_chunks import split_sequence, split_audio
-from koregraph.managers.choregraphy import save_choregaphy_chunk
-from koregraph.managers.audio import save_audio_chunk
-from koregraph.params import GENERATED_AUDIO_DIRECTORY, GENERATED_KEYPOINTS_DIRECTORY
+from koregraph.api.chunks import generate_chunk
 
 
 parser = ArgumentParser(
@@ -31,41 +26,13 @@ parser.add_argument(
 
 
 def main():
-    def clean_path(path, reload=True):
-        if os.path.exists(path) and not os.path.isfile(path) and reload:
-            try:
-                shutil.rmtree(path)
-            except Exception as e:
-                print(f"Failed to delete {path}. Reason: {e}")
-        path.mkdir(parents=True, exist_ok=True)
-
-    # Create folders where we will put the chunks
-    GENERATED_KEYPOINTS_DIRECTORY.mkdir(parents=True, exist_ok=True)
-    GENERATED_AUDIO_DIRECTORY.mkdir(parents=True, exist_ok=True)
-
     arguments = parser.parse_args()
 
     choregraphy_name = Path(arguments.choregraphy).stem
     chunk_size = arguments.chunk_size
     reload_music = arguments.reload_music
 
-    # Clean previous chunks out if needed
-    chore_path = GENERATED_KEYPOINTS_DIRECTORY / choregraphy_name / chunk_size
-    clean_path(chore_path)
-    _, _, _, _, music_name, _ = choregraphy_name.split("_")
-    music_path = GENERATED_AUDIO_DIRECTORY / music_name / chunk_size
-    clean_path(music_path, arguments.reload_music)
-
-    # Get and save chunks
-    chores = split_sequence(choregraphy_name, int(chunk_size))
-    for chore in chores:
-        save_choregaphy_chunk(chore, chore_path)
-
-    if reload_music or len(os.listdir(music_path)) == 0:
-        musics, sr = split_audio(choregraphy_name, chunk_size_sec=int(chunk_size))
-        for music, chunk_id in musics:
-            path = music_path / (music_name + f"_{chunk_id}.mp3")
-            save_audio_chunk(music, sr, path)
+    generate_chunk(choregraphy_name, chunk_size, reload_music)
 
 
 if __name__ == "__main__":
