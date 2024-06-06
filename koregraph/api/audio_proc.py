fa7b932c145708ac4cfa885f28c3866de3ead8bd
maudbenichou <34511790+MaudBenichou@users.noreply.github.com>
2024-06-06 11:45:26 +0200

    Update koregraph/api/audio_proc.py
    Co-authored-by: Anatole de Chauveron <54862351+Anatole-DC@users.noreply.github.com>

--- koregraph/api/audio_proc.py
+++ koregraph/api/audio_proc.py
@@ -1,6 +1,6 @@
 from koregraph.params import KEYPOINTS_DIRECTORY, FRAME_FORMAT, X_MIN, X_MAX
 
 
-def scale(X, X_min: float = X_MIN, X_max: float = X_MAX):
+def scale_audio(X, X_min: float = X_MIN, X_max: float = X_MAX):
     X_std = (X - X_min) / (X_max - X_min)
     return X_std
