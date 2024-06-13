e19a09f54d1227de2a7b4fafa3a25ffa386360c3
Anatole-DC <adechauveron@gmail.com>
2024-06-13 15:15:39 +0200

    Most Valuable Model

--- koregraph/api/machine_learning/neural_network.py
+++ koregraph/api/machine_learning/neural_network.py
@@ -36,15 +36,15 @@ def prepare_model(X, y) -> Model:
     return Sequential(
         [
             normalization_layer,
-            Bidirectional(LSTM(256, activation="relu", return_sequences=True)),
+            Bidirectional(LSTM(512, activation="relu", return_sequences=True)),
             # Bidirectional(LSTM(512, activation="relu", return_sequences=True)),
-            Bidirectional(LSTM(128, activation="relu")),
+            Bidirectional(LSTM(256, activation="relu")),
             # Bidirectional(LSTM(128, activation="relu")),
-            Dense(128, activation="relu"),
-            Dense(128, activation="relu"),
-            Dense(128, activation="relu"),
+            Dense(256, activation="relu"),
+            Dense(256, activation="relu"),
+            Dense(256, activation="relu"),
             Dropout(rate=0.2),
-            Dense(128, activation="relu"),
+            Dense(256, activation="relu"),
             Dropout(rate=0.2),
             Dense(y.shape[1], activation="sigmoid"),
         ]
